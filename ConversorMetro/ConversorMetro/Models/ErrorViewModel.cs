ccf1735de9692e81bb9d489915234eb5c0210fae
Lucas-O-S <lucasoscomercial@gmail.com>
2024-09-05 15:29:34 -0300

    Criação inicial

---
+++ ConversorMetro/ConversorMetro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConversorMetro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
